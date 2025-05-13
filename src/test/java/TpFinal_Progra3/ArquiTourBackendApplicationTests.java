d2e44bc64875f12ef1ecffe8300bc24382dfd48f
Matias Fresnadillo <167906583+mfresna@users.noreply.github.com>
2025-05-12 21:10:15 -0300

    Proyecto de Spring Initializr con las Dependencias

---
+++ src/test/java/TpFinal_Progra3/ArquiTourBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package TpFinal_Progra3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiTourBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

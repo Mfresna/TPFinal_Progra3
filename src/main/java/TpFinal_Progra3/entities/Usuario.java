4efdda3331b844c3a1ebb1d2dd15a46cf865c065
agusfuentes04 <afuentes0491@gmail.com>
2025-05-23 22:25:14 -0300

    Merge remote-tracking branch 'origin/main' Entidades

--- src/main/java/TpFinal_Progra3/entities/Usuario.java
+++ src/main/java/TpFinal_Progra3/entities/Usuario.java
@@ -1,4 +1,36 @@
 package TpFinal_Progra3.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private RolUsuario rol;
+
+    private String descripcion;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Imagen imagen;
+
+    private String nombre;
+    private String apellido;
+    private LocalDate fechaNacimiento;
+
+    private Boolean isActivo;
 }

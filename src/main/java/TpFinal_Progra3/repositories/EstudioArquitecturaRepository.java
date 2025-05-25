f94da6d001b9091d0d815796f7ff1a11a37ba11e
matias <matiasgfresnadillo@gmail.com>
2025-05-25 02:29:43 -0300

    package repositories

---
+++ src/main/java/TpFinal_Progra3/repositories/EstudioArquitecturaRepository.java
@@ -0,0 +1,10 @@
+//package TpFinal_Progra3.repositories;
+//
+//import TpFinal_Progra3.entities.EstudioArquitectura;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public class EstudioArquitecturaRepository extends JpaRepository<EstudioArquitectura, Long> {
+//
+//}
---
+++ src/main/java/TpFinal_Progra3/repositories/ImagenRepository.java
@@ -0,0 +1,10 @@
+//package TpFinal_Progra3.repositories;
+//
+//import TpFinal_Progra3.entities.Imagen;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public class ImagenRepository extends JpaRepository<Imagen,Long> {
+//
+//}

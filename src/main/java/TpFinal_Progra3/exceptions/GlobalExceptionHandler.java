45223bd9afb9c363c44cb4034708368c85aad61c
matias <matiasgfresnadillo@gmail.com>
2025-05-25 22:29:31 -0300

    Api Externa SendMail Excepciones Personalizadas Actualizacion de properties y env

---
+++ src/main/java/TpFinal_Progra3/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package TpFinal_Progra3.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailNoEnviadoException.class)
+    public ResponseEntity<String> handlerEnvioEmail(EmailNoEnviadoException e) {
+        if(e.getStatus() != null){
+            return ResponseEntity.status(e.getStatus()).body(e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(e.getMessage());
+    }
+
+}

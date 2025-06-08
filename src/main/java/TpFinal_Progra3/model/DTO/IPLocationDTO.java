9d0d4f1881a235fe7fe2396e3042e66c9b18df43
matias <matiasgfresnadillo@gmail.com>
2025-06-08 02:32:33 -0300

    Seguridad Deshabilitada Obra Service y Controller Basico

--- src/main/java/TpFinal_Progra3/model/DTO/IPLocationDTO.java
+++ src/main/java/TpFinal_Progra3/model/DTO/IPLocationDTO.java
@@ -1,5 +1,9 @@
 package TpFinal_Progra3.model.DTO;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,7 +11,16 @@ import lombok.Data;
 @Builder
 public class IPLocationDTO {
     //Recibe los Datos del JSON del service IPLocationService
+    @NotBlank(message = "La dirección IP no puede estar vacía o ser nula.")
     private String ip;
+
+    @NotNull(message = "La latitud no puede ser nula.")
+    @DecimalMin(value = "-90.0", inclusive = true, message = "La latitud debe ser mayor o igual a -90.0.")
+    @DecimalMax(value = "90.0", inclusive = true, message = "La latitud debe ser menor o igual a 90.0.")
     private double latitud;
+
+    @NotNull(message = "La longitud no puede ser nula.")
+    @DecimalMin(value = "-180.0", inclusive = true, message = "La longitud debe ser mayor o igual a -180.0.")
+    @DecimalMax(value = "180.0", inclusive = true, message = "La longitud debe ser menor o igual a 180.0.")
     private double longitud;
 }
--- src/main/java/TpFinal_Progra3/services/interfaces/ObraServiceInterface.java
+++ src/main/java/TpFinal_Progra3/services/interfaces/ObraServiceInterface.java
@@ -1,12 +1,13 @@
 package TpFinal_Progra3.services.interfaces;
 
+import TpFinal_Progra3.model.DTO.ObraDTO;
 import TpFinal_Progra3.model.entities.Obra;
 import java.util.List;
 import java.util.Optional;
 
 public interface ObraServiceInterface {
-    Obra crearObra(Obra obra);
-    Optional<Obra> obtenerObra(Long id);
-    List<Obra> listarObras();
+    ObraDTO crearObra(ObraDTO obra);
+    ObraDTO obtenerObra(Long id);
+    List<ObraDTO> listarObras();
     boolean eliminarObra(Long id);
 }
